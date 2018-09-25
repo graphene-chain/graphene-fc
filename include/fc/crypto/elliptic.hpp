56229d6d1fa4be989d74a28d93d77319d85eb786
Li RuiGang <yueyemingming@163.com>
2018-09-25 12:18:15 +0800

    update

--- include/fc/crypto/elliptic.hpp
+++ include/fc/crypto/elliptic.hpp
@@ -25,6 +25,8 @@ namespace fc {
     typedef fc::array<unsigned char,65> compact_signature;
     typedef std::vector<char>           range_proof_type;
     typedef fc::array<char,78>          extended_key_data;
+    typedef fc::sha256                  blinded_hash;    //liruigang20180925 sync fc
+    typedef fc::sha256                  blind_signature;       //liruigang20180925 sync fc
 
     /**
      *  @class public_key
@@ -157,6 +159,8 @@ namespace fc {
             fc::string to_base58() const { return str(); }
             static extended_public_key from_base58( const fc::string& base58 );
 
+            public_key generate_p( int i ) const;  //liruigang20180925 sync fc
+            public_key generate_q( int i ) const;  //liruigang20180925 sync fc
         private:
             sha256 c;
             int child_num, parent_fp;
@@ -183,9 +187,25 @@ namespace fc {
             static extended_private_key generate_master( const fc::string& seed );
             static extended_private_key generate_master( const char* seed, uint32_t seed_len );
 
-    private:
+            //liruigang20180925 sync fc
+            public_key blind_public_key( const extended_public_key& bob, int i ) const;
+            blinded_hash blind_hash( const fc::sha256& hash, int i ) const;
+            blind_signature blind_sign( const blinded_hash& hash, int i ) const;
+            // WARNING! This may produce non-canonical signatures!
+            compact_signature unblind_signature( const extended_public_key& bob,
+                                                 const blind_signature& sig,
+                                                 const fc::sha256& hash, int i ) const;
+
+        private:
             extended_private_key private_derive_rest( const fc::sha512& hash,
                                                       int num ) const;
+			//liruigang20180925 sync fc
+            private_key generate_a( int i ) const;
+            private_key generate_b( int i ) const;
+            private_key generate_c( int i ) const;
+            private_key generate_d( int i ) const;
+            private_key_secret compute_p( int i ) const;
+            private_key_secret compute_q( int i, const private_key_secret& p ) const;
             sha256 c;
             int child_num, parent_fp;
             uint8_t depth;
